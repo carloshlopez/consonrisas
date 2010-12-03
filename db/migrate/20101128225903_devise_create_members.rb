ae61db7477c9ade27c56c3f4864309fb06cf5ad8
Carlos <carloshlopez@gmail.com>
2010-12-02 22:03:30 -0500

    Members fix x

--- db/migrate/20101128225903_devise_create_members.rb
+++ db/migrate/20101128225903_devise_create_members.rb
@@ -16,8 +16,8 @@ class DeviseCreateMembers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :members, :email,                :unique => true
-    add_index :members, :reset_password_token, :unique => true
+#    add_index :members, :email,                :unique => true
+#    add_index :members, :reset_password_token, :unique => true
     # add_index :members, :confirmation_token,   :unique => true
     # add_index :members, :unlock_token,         :unique => true
   end
