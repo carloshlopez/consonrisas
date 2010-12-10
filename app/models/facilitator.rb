3ade4dc206196132b840aa7ed9645880ebefdfe8
Carlos <carloshlopez@gmail.com>
2010-12-09 23:31:27 -0500

    no validatiosn

--- app/models/facilitator.rb
+++ app/models/facilitator.rb
@@ -8,8 +8,8 @@ class Facilitator < ActiveRecord::Base
   has_attached_file :pic, :styles => {:profile => "150x150>", :thumb => "50x50#"}
 
 #  validates_attachment_presence :pic
-  validates_attachment_size :pic, :less_than => 5.megabytes
-  validates_attachment_content_type :pic, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+#  validates_attachment_size :pic, :less_than => 5.megabytes
+#  validates_attachment_content_type :pic, :content_type => ['image/jpeg', 'image/png', 'image/gif']
   
   def has_complete_info
     is_it = true
