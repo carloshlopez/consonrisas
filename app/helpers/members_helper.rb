52d8c8aa61b5a9a9e5d24f3ded6df6e8cf8b204b
Carlos <carloshlopez@gmail.com>
2011-03-28 22:59:12 -0500

    member helper for pic

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,18 @@
 module MembersHelper
+  def member_thumb(facilitator)
+    if facilitator.member and facilitator.member.use_facebook_pic and facilitator.member.facebook_id
+      "http://graph.facebook.com/#{facilitator.member.facebook_id}/picture"
+    else
+      facilitator.pic.url(:thumb)
+    end
+  end
+  
+  def member_pic(facilitator)
+    if facilitator.member.use_facebook_pic and facilitator.member.facebook_id
+      "http://graph.facebook.com/#{@member.facebook_id}/picture?type=large"
+    else 
+      facilitator.pic.url(:profile)
+    end
+  end
+  
 end
