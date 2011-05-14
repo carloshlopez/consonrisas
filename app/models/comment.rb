ba45c2165f0984bca53232864c556b551eb8513e
Carlos <carloshlopez@gmail.com>
2011-05-14 17:28:35 -0500

    emails for invite, event create and event comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,23 @@
 class Comment < ActiveRecord::Base
   belongs_to :event
   belongs_to :member
+  
+  after_create :email_comment
+  
+  def email_comment
+    self.event.facilitators.each do |facilitator|
+      EventInvitation.event_comment(self, facilitator.member.email, self.event).deliver      
+    end
+    self.event.fundations.each do |fundation|
+      fundation.fundation_admins.each do |admin|
+        EventInvitation.event_comment(self, admin.member.email, self.event).deliver if admin.active
+      end
+
+    end    
+    self.event.providers.each do |provider|
+      provider.provider_admins.each do |admin|
+        EventInvitation.event_comment(self, provider.member.email, self.event).deliver if admin.active
+      end
+    end        
+  end
 end
