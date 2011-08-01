3e723d0aba77919eb400878c15bb07ca31dd1331
Carlos <carloshlopez@gmail.com>
2011-08-01 18:38:38 -0500

    ssl omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'qfol8pqa9RUjJ6tR9p8fw', '70cxEdyf6FwhAHL3XUoyUXPd6dKsFxCUghWkpiiM'
-  provider :facebook, '161667273881845', 'a90fc797ce1572a3e4952b4d8172f228'
+  provider :facebook, '161667273881845', 'a90fc797ce1572a3e4952b4d8172f228',{:scope => 'email, offline_access', :client_options => {:ssl =>
+{:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
