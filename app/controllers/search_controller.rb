a32cd8e7f0391677ec0eb6ee20ae09339fb58779
Carlos <carloshlopez@gmail.com>
2011-08-01 22:58:04 -0500

    ordered search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,9 +14,19 @@ class SearchController < ApplicationController
     @members.each do |member|
       @facilitators << member.facilitator if member.facilitator
     end
+    @facilitators.sort! do |a,b| 
+      a.name.nil? ? -1 : b.name.nil? ? 1 : a.name.downcase <=> b.name.downcase 
+    end
+
     @providers = Provider.where("LOWER(name) like ?", "%#{params[:find].downcase}%")
+    @providers.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+
     @fundations = Fundation.where("LOWER(name) like ?", "%#{params[:find].downcase}%")
+    @fundations.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+
     @events = Event.where("LOWER(name) like ?", "%#{params[:find].downcase}%")
+    @events.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+
   end
 
 end
