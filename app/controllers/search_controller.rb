c85c8b004ddea67d611375eb3cf2883a1dd46e1c
Carlos <carloshlopez@gmail.com>
2011-08-01 22:27:43 -0500

    search fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,22 @@
 class SearchController < ApplicationController
   
   def find
-    @facilitators = Facilitator.where("name like ?", "%#{params[:find]}%")
     @members = Member.where("email like ?", "%#{params[:find]}%")
+    @facilitators = []
+    ids = []
+    @members.each do |member|
+      ids << member.id
+    end
+    
+
+    @facilitators = Facilitator.where("name like ? AND id NOT IN(?)", "%#{params[:find]}%", ids)
+    
     @members.each do |member|
       @facilitators << member.facilitator if member.facilitator
     end
     @providers = Provider.where("name like ?", "%#{params[:find]}%")
     @fundations = Fundation.where("name like ?", "%#{params[:find]}%")
     @events = Event.where("name like ?", "%#{params[:find]}%")
-    
   end
 
 end
