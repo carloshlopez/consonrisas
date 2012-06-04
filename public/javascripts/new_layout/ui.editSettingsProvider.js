1604d01b4b4e5e7d3ac136cc1ca0243cb6a2661a
Carlos <carloshlopez@gmail.com>
2012-06-04 17:55:48 -0500

    providers edit settings

--- public/javascripts/new_layout/ui.editSettingsProvider.js
+++ public/javascripts/new_layout/ui.editSettingsProvider.js
@@ -1,10 +1,10 @@
 jQuery(function() {
-	jQuery( "#tabs" ).tabs();
+	jQuery( "#tabs-provider" ).tabs();
 	
 	if(window.location.hash){
    var hash = window.location.hash.substring(1); 
-   if(hash == "my-profile"){
-    jQuery( "#tabs" ).tabs("enable", 1);
+   if(hash == "profile"){
+    jQuery( "#tabs-provider" ).tabs("enable", 1);
    }
 	}	
 	jQuery("html").scrollTop(10);
