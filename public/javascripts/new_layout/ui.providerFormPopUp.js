888a90abd3b529e7adead9f75e793eb06a22815d
Carlos <carloshlopez@gmail.com>
2012-06-14 15:25:08 -0500

    complete register ux improvement fixed no save on next step

--- public/javascripts/new_layout/ui.providerFormPopUp.js
+++ public/javascripts/new_layout/ui.providerFormPopUp.js
@@ -1,23 +1,29 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function(jQuery) {
       
-    $(".edit_provider").submit(function(e){
+    jQuery(".edit_provider").submit(function(e){
       e.preventDefault();
-      var form = $(this).closest("form");
-      var action = form.attr("action");
-      var serialized_form = form.serialize();
-      //console.log(action);
-      //console.log(serialized_form);      
-      action = action.substr(0,action.indexOf('?'));
-        $.post(action+".json", serialized_form, function(data){
-           if (data.resp == 'ok'){
-            $("#alert-msg p").html("Datos del Proveedor Actualizados");
-            $("#alert-msg").slideDown().delay(2500).slideUp();
-           }
-           else if(data.resp == 'error'){
-            $("#alert-msg p").html("Error actualizando los datos intenta más tarde");
-            $("#alert-msg").slideDown().delay(2500).slideUp();
-           }
-        });        
-     }); 
+      sendFormProvider();       
+     });
        
 });
+
+function sendFormProvider(doNotShowMessage){
+  var form = jQuery(".edit_provider").closest("form");
+  var action = form.attr("action");
+  var serialized_form = form.serialize();
+  action = action.substr(0,action.indexOf('?'));
+    jQuery.post(action+".json", serialized_form, function(data){
+       if (data.resp == 'ok'){
+         if(doNotShowMessage == null){
+          jQuery("#alert-msg p").html("Datos del Proveedor Actualizados");
+          jQuery("#alert-msg").slideDown().delay(2500).slideUp();
+        }
+       }
+       else if(data.resp == 'error'){
+        if(doNotShowMessage == null){
+          jQuery("#alert-msg p").html("Error actualizando los datos intenta más tarde");
+          jQuery("#alert-msg").slideDown().delay(2500).slideUp();
+        }
+       }
+    });
+}
