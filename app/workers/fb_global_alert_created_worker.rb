86781ea9c3c8248ba2193f03026de9d9e82dd57a
Carlos <carloshlopez@gmail.com>
2012-11-06 12:12:18 -0500

    fb worker iron io

--- app/workers/fb_global_alert_created_worker.rb
+++ app/workers/fb_global_alert_created_worker.rb
@@ -6,7 +6,7 @@ class FbGlobalAlertCreatedWorker < IronWorker::Base
 	def run
 
 		page = FbGraph::Page.new("207633962587548")
-		page.feed!(:access_token =>EVN['Fb_CS_TOKEN'], :link => link, :message => message)
+		page.feed!(:access_token =>'AAACTCRhuePUBAGW0okACn6o5nQjueWh3Ibwd3QeglNFM97iMeiQrOC4S7wJ7s7yA8JnwehH3jaBZAI0f08YkngMOjTkGzGrsZC8LZC5fn16niobwMkd', :link => link, :message => message)
 		
 	end
 
