0b94c7e9d31772a6ba72bf65a0ffbe77f2f8c727
Carlos <carloshlopez@gmail.com>
2013-01-23 15:21:28 -0500

    change to pg in dev, alert query fix for pg

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,7 +2,7 @@ class Alert < ActiveRecord::Base
   belongs_to :member
   validates :news, :presence => true
   def self.create_unique member_id, alert_type, link, role_id, news
-	unless Alert.where(:member_id => member_id, :alert_type=> alert_type, :link=> link, :role_id=> role_id).exists? then
+	unless Alert.where("link = ':link'", :member_id => member_id, :alert_type=> alert_type, :link=> link, :role_id=> role_id).exists? then
         begin 
           Alert.create!(:member_id=> member_id, :news=> news, :link=> link, :alert_type=> alert_type, :role_id=> role_id) 
           EventInvitation.invite_facilitator(Member.find(member_id), self).deliver 
