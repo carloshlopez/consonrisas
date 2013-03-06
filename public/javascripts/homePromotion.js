814a3c83901acd8090428e0f7f3a04b83b5c6b7c
Carlos <carloshlopez@gmail.com>
2013-03-06 17:57:55 -0500

    cirque promotion

---
+++ public/javascripts/homePromotion.js
@@ -0,0 +1,4 @@
+var $jq = jQuery.noConflict();
+$jq(document).ready(function($) {    
+  $jq("#show_promotions").dialog({modal:true, title:"Concurso: Circo del Sol", width: 480, closeText:"X", show:"clip"});  
+});
